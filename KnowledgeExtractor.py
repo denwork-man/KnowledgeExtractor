b8499727353d6be80c7c736ec5bd7e7d37c63ba2
Шевченко Никита Евгеньевич <Shevchenko.NE@dns-shop.ru>
2022-03-30 13:23:56 +1000

    wip

--- KnowledgeExtractor.py
+++ KnowledgeExtractor.py
@@ -5,11 +5,11 @@ class KnowledgeExtractor:
     # конструктор
     def __init__(self, inputFilePath, fTableFilePath, kTableFilePath, bTableFilePath, chTableFilePath):
         # запись данных Excel таблиц в поля класса
-        self.inputDataDf = pandas.read_excel(inputFilePath, sheet_name="Первичный осмотр")
-        self.fNameTableDf = pandas.read_excel(fTableFilePath, sheet_name="Лист1")
-        self.kNameAndNormTableDf = pandas.read_excel(kTableFilePath, sheet_name="Лист1")
-        self.bTimeCharacteristicTableDf = pandas.read_excel(bTableFilePath, sheet_name="Лист1")
-        self.chNameAndDigitNormTableDf = pandas.read_excel(chTableFilePath, sheet_name="Лист1")
+        self.inputData = pandas.read_excel(inputFilePath, sheet_name="Первичный осмотр").to_dict()
+        self.fNameTable = pandas.read_excel(fTableFilePath, sheet_name="Лист1").to_dict()
+        self.kNameAndNormTable = pandas.read_excel(kTableFilePath, sheet_name="Лист1").to_dict()
+        self.bTimeCharacteristicTable = pandas.read_excel(bTableFilePath, sheet_name="Лист1").to_dict()
+        self.chNameAndDigitNormTable = pandas.read_excel(chTableFilePath, sheet_name="Лист1").to_dict()
 
 
     def createRoughLikenessTable(self):
@@ -18,8 +18,8 @@ class KnowledgeExtractor:
         """
         worksheetRow = 0
         worksheetColumn = 0
-        roughLikenessRow = {}
         validColumns = []
+        roughLikenessData = []
         aviableColumns = [
             "БольЖив.Локализ",
             "Боль.Интенсивн",
@@ -41,23 +41,25 @@ class KnowledgeExtractor:
 
         # отбор тех колонок у которых значений заполнено более 40%
         for col in aviableColumns:
-            columnData = self.inputDataDf[col]
-            isValid = self.__isFillInPercent(columnData, 40)
+            columnData = self.inputData[col]
+            isValid = self.__isFillInPercent(columnData.values(), 40)
             if(isValid == True):
                 validColumns.append(col)
 
-        dataRowCount = self.inputDataDf.shape[0]
-
-        for dataRow in range(dataRowCount):
-            for dataColName in validColumns:
-                currentValue = self.inputDataDf[dataColName][dataRow]
-                kNamesAndNorms = self.__clearValues(self.kNameAndNormTableDf['название'])
-                chNameAndDigitNorms = self.__clearValues(self.chNameAndDigitNormTableDf['название'])
+        # ddd
+        for validColumnName in validColumns:
+            roughLikenessRow = {}
+            outItemsKeys = []
+            higherItemsKeys = []
+            lowerItemsKeys = []
+            for index, currentValue in self.inputData[validColumnName].items():
+                kNamesAndNorms = self.kNameAndNormTable['название'].values()
+                chNameAndDigitNorms = self.chNameAndDigitNormTable['название'].values()
 
                 # если колонка есть в числовых характеристиках
-                if dataColName in chNameAndDigitNorms.values:
-                    minValue = self.kNameAndNormTableDf.loc[self.kNameAndNormTableDf['название'] == dataColName]['Ниж гр нормы']
-                    maxValue = self.kNameAndNormTableDf.loc[self.kNameAndNormTableDf['название'] == dataColName]['Верх гран нормы']
+                if validColumnName in chNameAndDigitNorms:
+                    minValue = self.kNameAndNormTableDf.loc[self.kNameAndNormTableDf['название'] == validColumnName]['Ниж гр нормы']
+                    maxValue = self.kNameAndNormTableDf.loc[self.kNameAndNormTableDf['название'] == validColumnName]['Верх гран нормы']
                     if currentValue < minValue:
                         roughLikenessRow['lower'] = currentValue
                     elif currentValue > minValue:
@@ -66,22 +68,33 @@ class KnowledgeExtractor:
                         continue
 
                 # если колонка есть в качественных характеристиках
-                elif dataColName in kNamesAndNorms.values:
-                    normValue = self.kNameAndNormTableDf.loc[self.kNameAndNormTableDf['название'] == dataColName]['Норма (если есть)']
-                    if currentValue in normValue:
-                        roughLikenessRow['Out'] = col
-                    else:
+                elif validColumnName in kNamesAndNorms:
+                    key = [k for k, v in self.kNameAndNormTable['название'].items() if v == validColumnName][0]
+                    normValue = self.kNameAndNormTable['Норма (если есть)'][key]
+                    if not pandas.notnull(normValue):
                         continue
 
-                roughLikenessRow['obsNm'] = col
-                roughLikenessRow['Q-вне'] = len(roughLikenessRow['Out'])
-                roughLikenessRow['Q-high'] = len(roughLikenessRow['higher'])
-                roughLikenessRow['Q-low'] = len(roughLikenessRow['lower'])
-        
-        print(roughLikenessRow)
+                    if currentValue not in normValue:
+                        outItemsKeys.append(index + 2) # index == 0 это индекс в массиве, +2 что бы он стал как индекс в excel
+                    else:
+                        continue
+                else:
+                    break
+
+            if len(outItemsKeys) != 0 or len(higherItemsKeys) != 0 or len(lowerItemsKeys) != 0:
+                roughLikenessRow['ObsNm'] = validColumnName
+                roughLikenessRow['Out'] = ','.join(map(str, outItemsKeys)) or ''
+                roughLikenessRow['Q-Out'] = len(outItemsKeys) or ''
+                roughLikenessRow['Higher'] = ','.join(map(str, higherItemsKeys)) or ''
+                roughLikenessRow['Q-Higher'] = len(higherItemsKeys) or ''
+                roughLikenessRow['Lower'] = ','.join(map(str, lowerItemsKeys)) or ''
+                roughLikenessRow['Q-Lower'] = len(lowerItemsKeys) or ''
+                roughLikenessData.append(roughLikenessRow)
+
+        print(roughLikenessData)
+
         # workbook = xlsxwriter.Workbook('RoughLikenessTable.xlsx')
         # worksheet = workbook.add_worksheet()
-
         # worksheet.write(row, column, item)
         # row += 1
         # workbook.close()
@@ -95,7 +108,7 @@ class KnowledgeExtractor:
 
     def __isFillInPercent(self, data, precent):
         allCount = len(data)
-        isNullCount = data.isna().sum()
+        isNullCount = self.__getNanValuesCount(data)
         precentCount = round(precent * allCount / 100)
         # не меннее precent заполнено
         return (allCount - isNullCount) > precentCount
@@ -103,3 +116,10 @@ class KnowledgeExtractor:
     
     def __clearValues(self, data):
         return data.dropna().map(lambda item: item.rstrip()) # удаляет пустые и у оставшихся удаляет пробелы в конце
+    
+    def __getNanValuesCount(self, data):
+        nanCount = 0
+        for item in data:
+            if not pandas.notnull(item):
+                nanCount += 1
+        return nanCount
